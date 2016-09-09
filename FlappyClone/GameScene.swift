90a08a5f90ef6d9a7a10dff5ab3fa5728c010439
Hartrickb <Hartrickb@users.noreply.github.com>
2016-09-09 17:07:49 -0400

    create: new project as a flappy bird clone
    - Added graphics to the scene and positioned those graphics at a
    temporary position

--- FlappyClone/GameScene.swift
+++ FlappyClone/GameScene.swift
@@ -9,34 +9,51 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var ground = SKSpriteNode()
+    var ghost = SKSpriteNode()
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
         
-        self.addChild(myLabel)
+        ground = SKSpriteNode(imageNamed: "Ground")
+        ground.setScale(0.5)
+        ground.position = CGPoint(x: self.frame.width / 2, y: 0 + ground.frame.height / 2)
+        
+        self.addChild(ground)
+        
+        ghost = SKSpriteNode(imageNamed: "Ghost")
+        ghost.size = CGSize(width: 60, height: 70)
+        ghost.position = CGPoint(x: self.frame.width / 2 - ghost.frame.width, y: self.frame.height / 2)
+        
+        self.addChild(ghost)
+        
+        createWalls()
+        
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
+       
+    }
+    
+    func createWalls() {
         
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
+        let wallPair = SKNode()
+        let topWall = SKSpriteNode(imageNamed: "Wall")
+        let bottomWall = SKSpriteNode(imageNamed: "Wall")
+        
+        topWall.position = CGPoint(x: self.frame.width / 2, y: self.frame.height / 2 + 350)
+        bottomWall.position = CGPoint(x: self.frame.width / 2, y: self.frame.height / 2 - 350)
+        
+        topWall.setScale(0.5)
+        bottomWall.setScale(0.5)
+        
+        wallPair.addChild(topWall)
+        wallPair.addChild(bottomWall)
+        
+        self.addChild(wallPair)
+        
     }
    
     override func update(currentTime: CFTimeInterval) {
