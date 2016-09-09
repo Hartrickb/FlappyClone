75354288239174768696e5ecd2fd25708888fc6b
Hartrickb <Hartrickb@users.noreply.github.com>
2016-09-09 17:55:10 -0400

    feature: added physics to the game and minor bug fixes
    - Flipped top wall so that the green part shows as the bottom
    - Added physics so the ghost drops and collides with the ground and
    walls
    - Can only go vertical at the moment
    - Adjusted display depth of objects to make the ground appear in-front
    of walls

--- FlappyClone/GameScene.swift
+++ FlappyClone/GameScene.swift
@@ -8,6 +8,12 @@
 
 import SpriteKit
 
+struct PhysicsCategory {
+    static let Ghost: UInt32 = 0x1 << 1
+    static let Ground: UInt32 = 0x1 << 2
+    static let Wall: UInt32 = 0x1 << 3
+}
+
 class GameScene: SKScene {
     
     var ground = SKSpriteNode()
@@ -20,11 +26,31 @@ class GameScene: SKScene {
         ground.setScale(0.5)
         ground.position = CGPoint(x: self.frame.width / 2, y: 0 + ground.frame.height / 2)
         
+        ground.physicsBody = SKPhysicsBody(rectangleOfSize: ground.size)
+        ground.physicsBody?.categoryBitMask = PhysicsCategory.Ground
+        ground.physicsBody?.collisionBitMask = PhysicsCategory.Ghost
+        ground.physicsBody?.contactTestBitMask = PhysicsCategory.Ghost
+        ground.physicsBody?.affectedByGravity = false
+        ground.physicsBody?.dynamic = false
+        
+        // Ground appears infront of everything
+        ground.zPosition = 3
+        
         self.addChild(ground)
         
         ghost = SKSpriteNode(imageNamed: "Ghost")
         ghost.size = CGSize(width: 60, height: 70)
         ghost.position = CGPoint(x: self.frame.width / 2 - ghost.frame.width, y: self.frame.height / 2)
+        
+        ghost.physicsBody = SKPhysicsBody(circleOfRadius: ghost.frame.height / 2)
+        ghost.physicsBody?.categoryBitMask = PhysicsCategory.Ghost
+        ghost.physicsBody?.collisionBitMask = PhysicsCategory.Ground | PhysicsCategory.Wall
+        ghost.physicsBody?.contactTestBitMask = PhysicsCategory.Ground | PhysicsCategory.Wall
+        ghost.physicsBody?.affectedByGravity = true
+        ghost.physicsBody?.dynamic = true
+        
+        // Ghost appears in front of the wall but behind the ground
+        ghost.zPosition = 2
         
         self.addChild(ghost)
         
@@ -33,8 +59,11 @@ class GameScene: SKScene {
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
-       
+        /* Called when a touch begins */
+        
+        ghost.physicsBody?.velocity = CGVectorMake(0, 0)
+        ghost.physicsBody?.applyImpulse(CGVectorMake(0, 90))
+        
     }
     
     func createWalls() {
@@ -43,14 +72,33 @@ class GameScene: SKScene {
         let topWall = SKSpriteNode(imageNamed: "Wall")
         let bottomWall = SKSpriteNode(imageNamed: "Wall")
         
-        topWall.position = CGPoint(x: self.frame.width / 2, y: self.frame.height / 2 + 350)
-        bottomWall.position = CGPoint(x: self.frame.width / 2, y: self.frame.height / 2 - 350)
+        topWall.position = CGPoint(x: self.frame.width, y: self.frame.height / 2 + 350)
+        bottomWall.position = CGPoint(x: self.frame.width, y: self.frame.height / 2 - 350)
         
         topWall.setScale(0.5)
         bottomWall.setScale(0.5)
         
+        topWall.physicsBody = SKPhysicsBody(rectangleOfSize: topWall.size)
+        topWall.physicsBody?.categoryBitMask = PhysicsCategory.Wall
+        topWall.physicsBody?.collisionBitMask = PhysicsCategory.Ghost
+        topWall.physicsBody?.contactTestBitMask = PhysicsCategory.Ghost
+        topWall.physicsBody?.dynamic = false
+        topWall.physicsBody?.affectedByGravity = false
+        
+        bottomWall.physicsBody = SKPhysicsBody(rectangleOfSize: bottomWall.size)
+        bottomWall.physicsBody?.categoryBitMask = PhysicsCategory.Wall
+        bottomWall.physicsBody?.collisionBitMask = PhysicsCategory.Ghost
+        bottomWall.physicsBody?.contactTestBitMask = PhysicsCategory.Ghost
+        bottomWall.physicsBody?.dynamic = false
+        bottomWall.physicsBody?.affectedByGravity = false
+        
+        topWall.zRotation = CGFloat(M_PI)
+        
         wallPair.addChild(topWall)
         wallPair.addChild(bottomWall)
+        
+        // Sets walls to be behind the ground
+        wallPair.zPosition = 1
         
         self.addChild(wallPair)
         
